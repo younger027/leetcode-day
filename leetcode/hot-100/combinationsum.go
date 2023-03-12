95b43b1ec935bab358cd093371c53f5a86b1dca7
liuyangyang <liuyangyang@highlight.mobi>
2023-03-12 11:43:45 +0800

    add change

--- leetcode/hot-100/combinationsum.go
+++ leetcode/hot-100/combinationsum.go
@@ -69,3 +69,32 @@ func dfsCombination(candidates []int, begin, target int, result *[][]int, path *
 	}
 
 }
+// 主要在于递归中传递下一个数字
+var (
+	res [][]int
+	path  []int
+)
+func combinationSum(candidates []int, target int) [][]int {
+	res, path = make([][]int, 0), make([]int, 0, len(candidates))
+	sort.Ints(candidates)   // 排序，为剪枝做准备
+	dfsd(candidates, 0, target)
+	return res
+}
+
+func dfsd(candidates []int, start int, target int) {
+	if target == 0 {   // target 不断减小，如果为0说明达到了目标值
+		tmp := make([]int, len(path))
+		copy(tmp, path)
+		res = append(res, tmp)
+		return
+	}
+	for i := start; i < len(candidates); i++ {
+		if candidates[i] > target {  // 剪枝，提前返回
+			break
+		}
+		path = append(path, candidates[i])
+		dfsd(candidates, i, target - candidates[i])
+		path = path[:len(path) - 1]
+	}
+}
+
