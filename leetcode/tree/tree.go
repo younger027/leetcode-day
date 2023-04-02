e3fd0f5383316c9b42aad20419819c74a0c94068
liuyangyang <liuyangyang@highlight.mobi>
2023-04-02 23:26:55 +0800

    1 week tree

--- leetcode/tree/tree.go
+++ leetcode/tree/tree.go
@@ -4,6 +4,7 @@ import (
 	list2 "container/list"
 	"fmt"
 	leetcode "leetcode/leetcode/hot-100"
+	"math"
 )
 
 type TreeNode struct {
@@ -202,16 +203,13 @@ func rightSideView(root *TreeNode) []int {
 	nodeArray = append(nodeArray, root)
 	i := 0
 	for len(nodeArray) > 0 {
-		level := fmt.Sprintf("level %d, value:", i)
-		for index, tNode := range nodeArray {
-			level += fmt.Sprintf("%d", tNode.Value)
+		result = append(result, nodeArray[len(nodeArray)-1].Value)
 
+		for i := range nodeArray {
+			fmt.Println(i)
 			node := nodeArray[0]
 			nodeArray = nodeArray[1:]
-			if index == len(nodeArray)-1 {
-				result = append(result, tNode.Value)
-			}
-			
+
 			if node.Left != nil {
 				nodeArray = append(nodeArray, node.Left)
 			}
@@ -221,7 +219,6 @@ func rightSideView(root *TreeNode) []int {
 			}
 		}
 
-		fmt.Println(level)
 		i += 1
 	}
 
@@ -264,4 +261,266 @@ func PreorderTraverse(root *TreeNode) []int {
 	}
 
 	return result
+}
+
+//中序遍历的递归写法
+func MiddleTraverseRecursive(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+
+	result = append(result, MiddleTraverseRecursive(root.Left)...)
+	result = append(result, root.Value)
+	result = append(result, MiddleTraverseRecursive(root.Right)...)
+
+	return result
+}
+
+func MiddleTraverse(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+
+	stack := list2.New()
+	for stack.Len() > 0 || root != nil {
+		if root != nil {
+			stack.PushBack(root)
+			fmt.Println("PushBack node---", stack.Len())
+
+			root = root.Left
+		} else {
+			root = stack.Remove(stack.Back()).(*TreeNode)
+			fmt.Println("node---", root.Value)
+			result = append(result, root.Value)
+			root = root.Right
+
+		}
+	}
+
+	return result
+}
+
+//后序遍历的递归写法
+func PostTraverseRecursive(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+
+	result = append(result, PostTraverseRecursive(root.Left)...)
+	result = append(result, PostTraverseRecursive(root.Right)...)
+	result = append(result, root.Value)
+
+	return result
+}
+
+func PostTraverseTraverse(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+
+	stack := list2.New()
+	stack.PushBack(root)
+	for stack.Len() > 0 {
+		root = stack.Remove(stack.Back()).(*TreeNode)
+		result = append(result, root.Value)
+		if root.Left != nil {
+			stack.PushBack(root.Left)
+		}
+
+		if root.Right != nil {
+			stack.PushBack(root.Right)
+		}
+	}
+
+	reverse(result)
+	return result
+}
+
+func reverse(data []int) {
+	for i := 0; i < len(data)/2; i++ {
+		data[i], data[len(data)-i-1] = data[len(data)-i-1], data[i]
+	}
+}
+
+//637.二叉树的层平均值
+func averageOfLevels(root *TreeNode) []float64 {
+	result := make([]float64, 0)
+	if root == nil {
+		return result
+	}
+
+	levelArray := make([]*TreeNode, 0)
+	levelArray = append(levelArray, root)
+	for len(levelArray) > 0 {
+		levelTotal := 0
+		sz := len(levelArray)
+		for i := 0; i < sz; i++ {
+			tail := levelArray[0]
+			levelArray = levelArray[1:]
+
+			levelTotal += tail.Value
+			if tail.Left != nil {
+				levelArray = append(levelArray, tail.Left)
+			}
+
+			if tail.Right != nil {
+				levelArray = append(levelArray, tail.Right)
+			}
+		}
+
+		result = append(result, float64(levelTotal)/float64(sz))
+	}
+
+	return result
+}
+
+//429.N叉树的层序遍历
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func levelOrder(root *Node) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	levelArray := make([]*Node, 0)
+	levelArray = append(levelArray, root)
+	for len(levelArray) > 0 {
+		var tmp []int
+		sz := len(levelArray)
+		for i := 0; i < sz; i++ {
+			node := levelArray[0]
+			levelArray = levelArray[1:]
+			tmp = append(tmp, node.Val)
+
+			for _, v := range node.Children {
+				levelArray = append(levelArray, v)
+			}
+		}
+		result = append(result, tmp)
+	}
+
+	return result
+}
+
+//515. 在每个树行中找最大值
+func largestValues(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+
+	levelArray := make([]*TreeNode, 0)
+	levelArray = append(levelArray, root)
+	for len(levelArray) > 0 {
+		sz := len(levelArray)
+		max := math.MinInt64
+		for i := 0; i < sz; i++ {
+			node := levelArray[0]
+			levelArray = levelArray[1:]
+			if node.Value > max {
+				max = node.Value
+			}
+			if node.Left != nil {
+				levelArray = append(levelArray, node.Left)
+			}
+			if node.Right != nil {
+				levelArray = append(levelArray, node.Right)
+
+			}
+		}
+
+		result = append(result, max)
+	}
+
+	return result
+}
+
+type NewNode struct {
+	Val   int
+	Left  *NewNode
+	Right *NewNode
+	Next  *NewNode
+}
+
+//116. 填充每个节点的下一个右侧节点指针
+func connect(root *NewNode) *NewNode {
+	if root == nil {
+		return root
+	}
+
+	levelArray := make([]*NewNode, 0)
+	levelArray = append(levelArray, root)
+
+	for len(levelArray) > 0 {
+		sz := len(levelArray)
+		for i := 0; i < sz; i++ {
+			node := levelArray[0]
+			levelArray = levelArray[1:]
+			if node.Left != nil {
+				levelArray = append(levelArray, node.Left)
+			}
+			if node.Right != nil {
+				levelArray = append(levelArray, node.Right)
+
+			}
+
+			if i < sz-1 {
+				node.Next = levelArray[0]
+			}
+
+		}
+	}
+
+	return root
+}
+
+//226. 翻转二叉树
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	root.Left, root.Right = root.Right, root.Left
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
+
+func invertTree2(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	stack := list2.New()
+	stack.PushBack(root)
+	for stack.Len() > 0 {
+		node := stack.Remove(stack.Back()).(*TreeNode)
+		node.Left, node.Right = node.Right, node.Left
+
+		if node.Left != nil {
+			stack.PushBack(node.Left)
+		}
+
+		if node.Right != nil {
+			stack.PushBack(node.Right)
+		}
+	}
+
+	return root
 }
--- leetcode/tree/tree_test.go
+++ leetcode/tree/tree_test.go
@@ -9,7 +9,6 @@ func TestFindInitBinaryTree(t *testing.T) {
 
 	t.Log(levelOrderBottom(root))
 
-	t.Log(rightSideView(root))
 	LevelTraverse(root)
 
 	t.Log("PreorderTraverseRecursive-", PreorderTraverseRecursive(root))
@@ -25,10 +24,21 @@ func TestDiameterOfBinaryTree(t *testing.T) {
 }
 
 func TestPreorderTraverse(t *testing.T) {
-	data := []int{3, 9, 20, -1, -1, 15, 7}
+	data := []int{1, 2, 3, 6, 5, 9, 4}
 	root := InitBinaryTree(data, 0)
 	LevelTraverse(root)
 
-	t.Log(PreorderTraverse(root))
+	//t.Log(PreorderTraverse(root))
+	//t.Log(rightSideView(root))
 
+	//t.Log(MiddleTraverseRecursive(root))
+	//t.Log(MiddleTraverse(root))
+
+	//t.Log(PostTraverseRecursive(root))
+	//t.Log(PostTraverseTraverse(root))
+
+	//t.Log(largestValues(root))
+
+	invertTree2(root)
+	LevelTraverse(root)
 }
